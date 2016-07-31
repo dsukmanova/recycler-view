881540257385b51400121fa937b727c3d2e81966
dsukmanova <sukmanovadarya@yandex.ru>
2016-07-31 15:42:40 +0300

    change background color from white to red

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/MyItemTochHelperCallback.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/MyItemTochHelperCallback.java
@@ -1,7 +1,11 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 
 /**
@@ -32,5 +36,23 @@ public class MyItemTochHelperCallback extends ItemTouchHelper.Callback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         int position = viewHolder.getAdapterPosition();
         myItemTouchHelperAdapter.onItemDissmiss(position);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View itemView = viewHolder.itemView;
+            int itemViewWidth = itemView.getWidth();
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            int maxAlpha = 255;
+            double kAlpha = 0.75;
+            paint.setAlpha(Math.min(maxAlpha, (int) (kAlpha * maxAlpha * Math.abs(dX) / itemViewWidth)));
+            float rectLeft = dX > 0 ? itemView.getLeft() : (itemView.getLeft() + dX);
+            float rectRight = dX > 0 ? dX : itemView.getRight();
+            c.drawRect(rectLeft, itemView.getTop(), rectRight, itemView.getBottom(), paint);
+
+        }
     }
 }
