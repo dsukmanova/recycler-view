bde78fb6ce1ee386483ec6c8362ef8e02b02291b
dsukmanova <sukmanovadarya@yandex.ru>
2016-07-31 17:42:43 +0300

    add menu

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +17,11 @@ import butterknife.BindView;
 import ru.yandex.yamblz.R;
 
 public class ContentFragment extends BaseFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @BindView(R.id.rv)
     RecyclerView rv;
@@ -22,6 +30,21 @@ public class ContentFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_content, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId){
+            case R.id.borders:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -34,6 +57,8 @@ public class ContentFragment extends BaseFragment {
         ItemTouchHelper.Callback callback = new MyItemTochHelperCallback(contentAdapter);
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
         itemTouchHelper.attachToRecyclerView(rv);
+
+        //rv.addItemDecoration(new MyItemDecoration());
 
     }
 }
